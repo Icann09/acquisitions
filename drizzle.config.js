252309d1159363fe21809cf6a9cf505d8f64b0db
Ican09 <kaisanfarasdag09@gmail.com>
2025-11-08 03:17:33 +0800

    implement dockerization

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,7 @@ export default {
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
-  }
-}; 
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
+  },
+};
